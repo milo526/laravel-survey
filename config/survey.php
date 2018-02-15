4fcd074d9d5242f3fa68aac355b7be4fe2924d6e
Milo Cesar <milocesar1@gmail.com>
2018-02-15 23:18:25 +0100

    Added config

---
+++ config/survey.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    'models' => [
+
+        /*
+         * We need to know which Eloquent model should be used to retrieve your categories.
+         * Of course, it is often just the "Category" model but you may use whatever you like.
+         *
+         * The model you want to use as a Category model needs to implement the
+         * `MCesar\Survey\Contracts\Category` contract.
+         */
+
+        'category' => MCesar\Survey\Models\Category::class,
+
+        /*
+         * We need to know which Eloquent model should be used to retrieve your questions.
+         * Of course, it is often just the "Question" model but you may use whatever you like.
+         *
+         * The model you want to use as a Question model needs to implement the
+         * `MCesar\Survey\Question\Category` contract.
+         */
+
+        'question' => MCesar\Survey\Models\Question::class,
+
+        /*
+         * We need to know which Eloquent model should be used to retrieve your answers.
+         * Of course, it is often just the "Answer" model but you may use whatever you like.
+         *
+         * The model you want to use as a Answer model needs to implement the
+         * `MCesar\Survey\Question\Answer` contract.
+         */
+
+        'answer' => MCesar\Survey\Models\Answer::class,
+
+    ],
+];
--- src/SurveyServiceProvider.php
+++ src/SurveyServiceProvider.php
@@ -3,22 +3,47 @@
 namespace MCesar\Survey;
 
 use Illuminate\Support\ServiceProvider;
+use MCesar\Survey\Contracts\Answer as AnswerContract;
+use MCesar\Survey\Contracts\Category as CategoryContract;
+use MCesar\Survey\Contracts\Question as QuestionContract;
 
 class SurveyServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if (! class_exists('CreateSurveyTables')) {
-            $timestamp = date('Y_m_d_His', time());
-
+        if (isNotLumen()) {
             $this->publishes([
-                __DIR__.'/../database/migrations/create_survey_tables.php.stub' => $this->app->databasePath()."/migrations/{$timestamp}_create_survey_tables.php",
-            ], 'migrations');
+                __DIR__ . '/../config/survey.php' => config_path('survey.php'),
+            ], 'config');
+
+            if (!class_exists('CreateSurveyTables')) {
+                $timestamp = date('Y_m_d_His', time());
+
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/create_survey_tables.php.stub' => $this->app->databasePath() . "/migrations/{$timestamp}_create_survey_tables.php",
+                ], 'migrations');
+            }
         }
+
+        $this->registerModelBindings();
     }
 
     public function register()
     {
+        if (isNotLumen()) {
+            $this->mergeConfigFrom(
+                __DIR__.'/../config/survey.php',
+                'survey'
+            );
+        }
+    }
 
+    protected function registerModelBindings()
+    {
+        $config = $this->app->config['survey.models'];
+
+        $this->app->bind(CategoryContract::class, $config['category']);
+        $this->app->bind(QuestionContract::class, $config['question']);
+        $this->app->bind(AnswerContract::class, $config['answer']);
     }
 }
