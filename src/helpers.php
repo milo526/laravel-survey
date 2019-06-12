6285e6cc4fff7929734bcc73e0d3fe08059de376
pxgamer <owzie123@gmail.com>
2019-06-12 15:26:17 +0100

    Update to use a namespace for the helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,13 +1,13 @@
 <?php
 
-if (! function_exists('isNotLumen')) {
-    /**
-     * @param string $guard
-     *
-     * @return string|null
-     */
-    function isNotLumen() : bool
-    {
-        return ! preg_match('/lumen/i', app()->version());
-    }
+namespace MCesar\Survey;
+
+/**
+ * @param string $guard
+ *
+ * @return string|null
+ */
+function isNotLumen() : bool
+{
+    return ! preg_match('/lumen/i', app()->version());
 }
